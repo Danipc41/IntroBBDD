9930b0a138227367910f1325b96979f62d62e42a
Danibm <danibenitod@gmail.com>
2025-09-27 16:42:43 +0200

    Add files via upload

---
+++ Intro/introBBDD.sql
@@ -0,0 +1,16 @@
+--1 
+SELECT flight_id, flight_no, status FROM flights WHERE status = 'On Time';
+
+--2 
+SELECT * FROM bookings WHERE total_amount >= 1000000;
+
+--3
+SELECT * FROM aircrafts_data
+
+4--
+SELECT flight_id, flight_no FROM flights WHERE aircraft_code = '737'
+
+5--
+SELECT * FROM tickets WHERE passenger_name LIKE 'IRINA%';
+
+6--
